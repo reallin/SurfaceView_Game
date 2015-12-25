b010303c27bfa99b4912e068a835d743e974c741
linxj <635596741@qq.com>
2015-12-25 12:35:49 +0800

    commit the project

---
+++ src/com/lxj/spirt/Sprite.java
@@ -0,0 +1,28 @@
+package com.lxj.spirt;
+
+import android.R.drawable;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Point;
+
+
+public abstract class Sprite {
+	// Õº∆¨
+	protected Bitmap img;
+	// Œª÷√
+	protected Point pos;
+	public Sprite(Bitmap i,Point p){
+		this.img = i;
+		this.pos = p;
+		if(this.pos == null){
+			this.pos = new Point(0,0);
+		}
+	}
+	public void draw(Canvas canvas){
+		if(img != null)
+		canvas.drawBitmap(this.img, this.pos.x, this.pos.y, null);
+	}
+	public Point getPoint(){
+		return this.pos;
+	}
+}
